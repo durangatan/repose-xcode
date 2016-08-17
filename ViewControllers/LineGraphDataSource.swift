4aa01ebd586af821b7780775a43e70b37c9e9a3b
Joe Duran <joeduran8@gmail.com>
2016-08-17 09:04:15 -0500

    fix merge conflicts

--- ViewControllers/LineGraphDataSource.swift
+++ ViewControllers/LineGraphDataSource.swift
@@ -11,7 +11,7 @@ import ResearchKit
 import Alamofire
 
 
-class LineGraphDataSource: NSObject, ORKGraphChartViewDataSource {
+class LineGraphDataSource: NSObject {
         
     var plotPoints:[[ORKRangedPoint]] = []
 
