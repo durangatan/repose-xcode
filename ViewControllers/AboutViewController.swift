32f847013e2ab0f26199107d99c5c761ce35b33b
Joe Duran <joeduran8@gmail.com>
2016-08-17 20:48:11 -0500

    add profile web view

--- ViewControllers/AboutViewController.swift
+++ ViewControllers/AboutViewController.swift
@@ -9,6 +9,33 @@
 import UIKit
 
 
-class AboutViewController: UIViewController {
+class AboutViewController: UIViewController, UIWebViewDelegate {
 
+    @IBOutlet weak var web: UIWebView!
+    
+    var indicator: UIActivityIndicatorView = UIActivityIndicatorView(activityIndicatorStyle: UIActivityIndicatorViewStyle.Gray)
+
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        indicator.frame = CGRectMake(0.0, 0.0, 40.0, 40.0);
+        indicator.center = view.center
+        view.addSubview(indicator)
+        indicator.bringSubviewToFront(view)
+        UIApplication.sharedApplication().networkActivityIndicatorVisible = true
+        let url = NSURL(string: "https://repose.herokuapp.com");
+        let requestObj = NSURLRequest(URL: url!);
+        web.loadRequest(requestObj);
+        requestObj
+    }
+    
+    func webViewDidStartLoad(webView: UIWebView){
+        indicator.startAnimating()
+        
+    }
+    public func webViewDidFinishLoad(webView: UIWebView){
+        indicator.stopAnimating()
+    }
 }
