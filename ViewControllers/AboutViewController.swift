757282be1199c623c292763ead90eecc6442cb0e
Joe Duran <joeduran8@gmail.com>
2016-08-17 20:31:45 -0500

    add view controller

---
+++ ViewControllers/AboutViewController.swift
@@ -0,0 +1,14 @@
+//
+//  AboutViewController.swift
+//  Repose
+//
+//  Created by Joseph Duran on 8/17/16.
+//  Copyright © 2016 Repo Men. All rights reserved.
+//
+
+import UIKit
+
+
+class AboutViewController: UIViewController {
+
+}
